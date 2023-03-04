82871bd7036cc0b33afab3fe094ba58fd5519aac
reiko <125785087+acreiko@users.noreply.github.com>
2023-03-04 11:29:07 +0000

    Update servemedia.py
    removed unneccesary error printing statement

--- src/app/functions/media/servemedia.py
+++ src/app/functions/media/servemedia.py
@@ -71,7 +71,6 @@ async def servemedia(contentid):
 
     except (Exception, psycopg2.DatabaseError):
         time_task_took = time.time() - task_start_time
-        print(str(traceback.format_exc()))
         await logErrorToDB(str(traceback.format_exc()), timetaken=time_task_took)
         raise HTTPException(
             status_code=500,
