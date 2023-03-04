e241b60133919cf3fce7ab5943bd5a08ebee3586
reiko <125785087+acreiko@users.noreply.github.com>
2023-03-04 06:56:52 +0000

    searches users and posts more efficiently. now with pages.

--- src/app/functions/meilisearch/search.py
+++ src/app/functions/meilisearch/search.py
@@ -2,11 +2,25 @@ import meilisearch
 from ..dbconfig import meilisearchConfig
 
 
-def MSSearchPosts(searchterm):
+def MSSearchPosts(searchterm: str, page: int):
     msdb = meilisearchConfig()[0]
     msdbkey = meilisearchConfig()[1]
     client = meilisearch.Client(msdb, msdbkey)
 
     index = client.index('posts')
-    result = index.search(searchterm)
+    result = index.search(searchterm, {
+        'offset': page
+    })
+    return result
+
+
+def MSSearchUsers(searchterm: str, page: int):
+    msdb = meilisearchConfig()[0]
+    msdbkey = meilisearchConfig()[1]
+    client = meilisearch.Client(msdb, msdbkey)
+
+    index = client.index('users')
+    result = index.search(searchterm, {
+        'offset': page
+    })
     return result
--- src/app/routers/search.py
+++ src/app/routers/search.py
@@ -1,10 +1,16 @@
 from fastapi import APIRouter
 
 from functions.meilisearch.search import MSSearchPosts
+from functions.meilisearch.search import MSSearchUsers
 
 router = APIRouter()
 
 
 @router.get('/search/posts', tags=["search"])
-def searchPosts(searchterm):
-    return MSSearchPosts(searchterm)
+def searchPosts(searchterm: str, page: int):
+    return MSSearchPosts(searchterm, page)
+
+
+@router.get('/search/users', tags=["search"])
+def searchUsers(searchterm: str, page: int):
+    return MSSearchUsers(searchterm, page)
