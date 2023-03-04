92084e436b5202f09d5b6b00d126fcf93f4e7f6d
reiko <125785087+acreiko@users.noreply.github.com>
2023-03-04 02:39:06 +0000

    Update MSIndex.py
    indexing posts now works. it used to not due to a bug where id and authorid were getting mixed up by MS

--- src/app/functions/meilisearch/MSIndex.py
+++ src/app/functions/meilisearch/MSIndex.py
@@ -6,17 +6,22 @@ import json
 from ..dbconfig import meilisearchConfig
 
 
-async def IndexPost(postid, authorid, authorusername, title, textcontent, media, timestamp, topic):
+async def IndexPost(postid: int, authorid: int, authorusername: str, title: str, textcontent: str,
+                    media: str, timestamp: int, topic: str):
     msdb = meilisearchConfig()[0]
     msdbkey = meilisearchConfig()[1]
     client = meilisearch.Client(msdb, msdbkey)
 
-    index = client.index('posts')
-
-    document = [
-        {'id': postid, 'author_id': authorid, 'author_username': authorusername, 'post_title': title,
-         'text_content': textcontent, 'media_ids': media, 'upload_timestamp': timestamp, 'topic': topic,
-         },
-    ]
-
-    index.add_documents(document)
+    client.index('posts').add_documents([{
+        'id': int(postid),
+        'authorid': str(authorid),
+        'author': authorusername,
+        'title': title,
+        'content': textcontent,
+        'media': media,
+        'timestamp': str(timestamp),
+        'topic': topic
+    }],
+        'id'  # primary key for MS posts.
+        # this is so MS can understand how many posts there are and how to identify one from another.
+    )
