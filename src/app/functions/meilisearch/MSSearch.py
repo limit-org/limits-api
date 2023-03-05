40a0988c88d417379d531109893ad9ac24aafdcd
reiko <125785087+acreiko@users.noreply.github.com>
2023-03-05 15:46:19 +0000

    added comments

--- src/app/functions/meilisearch/MSSearch.py
+++ src/app/functions/meilisearch/MSSearch.py
@@ -2,25 +2,27 @@ import meilisearch
 from ..dbconfig import meilisearchConfig
 
 
+# func for searching posts
 def MSSearchPosts(searchterm: str, page: int):
     msdb = meilisearchConfig()[0]
     msdbkey = meilisearchConfig()[1]
     client = meilisearch.Client(msdb, msdbkey)
 
-    index = client.index('posts')
+    index = client.index('posts')  # set index to posts
     result = index.search(searchterm, {
         'offset': page
-    })
-    return result
+    })  # search for {searchterm} on page {page}
+    return result  # return search results
 
 
+# func for searching for/about users
 def MSSearchUsers(searchterm: str, page: int):
     msdb = meilisearchConfig()[0]
     msdbkey = meilisearchConfig()[1]
     client = meilisearch.Client(msdb, msdbkey)
 
-    index = client.index('users')
+    index = client.index('users')  # set index to users
     result = index.search(searchterm, {
         'offset': page
-    })
-    return result
+    })  # search for {searchterm} on page {page}
+    return result  # return search results
