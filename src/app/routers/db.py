e202e4e1b101f75cbc1c0bc86691cff2d144a7e9
acreiko <acreiko@protonmail.com>
2023-03-07 22:25:29 +0000

    Update db.py
    added a status code.

--- src/app/routers/db.py
+++ src/app/routers/db.py
@@ -1,11 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from functions.db import returnVersion
 
 router = APIRouter()
 
 
-@router.get("/db/version", tags=["db"])
+@router.get("/db/version", tags=["db"], status_code=200)
 async def get_db_version():
     return await returnVersion()
     # return "This endpoint is potentially dangerous in a production environment: It has been disabled."
